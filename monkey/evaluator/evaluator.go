f68177e98a000b63ee7afa38a95decf8d1cda8cd
ndaDayo <hayato711+nda@gmail.com>
2023-06-23 21:53:53 +0900

    [add] eval bang operator expression

--- monkey/evaluator/evaluator.go
+++ monkey/evaluator/evaluator.go
@@ -47,3 +47,25 @@ func nativeBooleanObject(input bool) *object.Boolean {
 
 	return FALSE
 }
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
+}
