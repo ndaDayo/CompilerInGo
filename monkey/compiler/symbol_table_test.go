8ab17c855c18c2d13dfef66d6d66db739f4661b2
ndaDayo <hayato711+nda@gmail.com>
2023-07-29 22:06:27 +0900

    [add] TestDefine

---
+++ monkey/compiler/symbol_table_test.go
@@ -0,0 +1,24 @@
+package compiler
+
+import (
+	"testing"
+)
+
+func TestDefine(t *testing.T) {
+	expected := map[string]Symbol{
+		"a": {Name: "a", Scope: GlobalScope, Index: 0},
+		"b": {Name: "b", Scope: GlobalScope, Index: 1},
+	}
+
+	global := NewSymbolTable()
+
+	a := global.Define("a")
+	if a != expected["a"] {
+		t.Errorf("expected a=%+v, got=%+v", expected["a"], a)
+	}
+
+	b := global.Define("b")
+	if a != expected["b"] {
+		t.Errorf("expected b=%+v, got=%+v", expected["b"], b)
+	}
+}
