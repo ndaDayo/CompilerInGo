02c9a5bc346a17bc9101418640097f26b907864a
ndaDayo <hayato711+nda@gmail.com>
2023-09-09 22:16:17 +0900

    add: compile func literal return value

--- monkey/compiler/compiler_test.go
+++ monkey/compiler/compiler_test.go
@@ -27,7 +27,6 @@ func TestCompilerScopes(t *testing.T) {
 	if compiler.scopeIndex != 0 {
 		t.Errorf("scopeIndex wrong. got=%d, want=%d", compiler.scopeIndex, 0)
 	}
-	globalSymbolTable := compiler.symbolTable
 
 	compiler.emit(code.OpMul)
 
@@ -49,21 +48,10 @@ func TestCompilerScopes(t *testing.T) {
 			last.Opcode, code.OpSub)
 	}
 
-	if compiler.symbolTable.Outer != globalSymbolTable {
-		t.Errorf("compiler did not enclose symbolTable")
-	}
-
 	compiler.leaveScope()
 	if compiler.scopeIndex != 0 {
 		t.Errorf("scopeIndex wrong. got=%d, want=%d",
 			compiler.scopeIndex, 0)
-	}
-
-	if compiler.symbolTable != globalSymbolTable {
-		t.Errorf("compiler did not restore global symbol table")
-	}
-	if compiler.symbolTable.Outer != nil {
-		t.Errorf("compiler modified global symbol table incorrectly")
 	}
 
 	compiler.emit(code.OpAdd)
