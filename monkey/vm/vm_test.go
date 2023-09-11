7d51e8c1d36146a270deba3896118904aeb4c820
ndaDayo <hayato711+nda@gmail.com>
2023-09-11 22:28:19 +0900

    add: return statement test

--- monkey/vm/vm_test.go
+++ monkey/vm/vm_test.go
@@ -334,3 +334,24 @@ func TestCallingFunctionsWithoutArguments(t *testing.T) {
 
 	runVmTests(t, tests)
 }
+
+func TestFunctionsWithReturnStatement(t *testing.T) {
+	tests := []vmTestCase{
+		{
+			input: `
+		let earlyExit = fn() { return 99; 100; };
+		earlyExit();
+		`,
+			expected: 99,
+		},
+		{
+			input: `
+		let earlyExit = fn() { return 99; return 100; };
+		earlyExit();
+		`,
+			expected: 99,
+		},
+	}
+
+	runVmTests(t, tests)
+}
